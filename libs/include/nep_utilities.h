556505af8b8140ab891a2bda59602b6bd57d5757
bhk <b13548849360@gmail.com>
2025-03-08 10:16:53 +0800

    add lib folders and files

---
+++ libs/include/nep_utilities.h
@@ -0,0 +1,12 @@
+/*
+    Copyright 2025 Zheyong Fan, Junjie Wang, Eric Lindgren, Hekai Bu
+    NEP_CPU is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with NEP_CPU.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "nep.h"
